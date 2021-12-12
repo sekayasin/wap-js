47353deca614ec480b8d2af305dde5fe4c27e37f
sekayasin <sekayasin@gmail.com>
2021-12-12 01:03:41 -0600

    - Add Lab10

---
+++ lab10/lab10.js
@@ -0,0 +1,21 @@
+/*
+Question 1
+Write the necessary Node script to make this code work for all arrays: 
+[1,2,3,4,5,6,7,8].even(); // [2,4,6,8] 
+[1,2,3,4,5,6,7,8].odd(); // [1,3,5,7] 
+
+Test your code in Node.JS CLI
+*/
+
+Array.prototype.even = function() {
+    let arr = this;
+    return arr.filter(num => num % 2 === 0);
+};
+Array.prototype.odd = function() {
+    let arr = this;
+    return arr.filter(num => num % 2 === 1);
+};
+console.log([5,2,3,11,0,9].even());
+console.log([5,2,3,11,0,9].odd());
+console.log([1,2,3,4,5,6,7,8].even())
+console.log([1,2,3,4,5,6,7,8].odd())
